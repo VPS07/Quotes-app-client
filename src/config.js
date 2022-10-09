800c930d2a3a3def114b722872431f99a4f95e6b
vps07 <vikysingh07860@gmail.com>
2022-10-09 23:37:00 +0530

    Updated config file

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-export const axiosInstance = axios.create({
+const axiosInstance = axios.create({
   baseURL: "https://quote-web-app.onrender.com",
 });
+
+export default axiosInstance;
