ad8cacb958b9fa52ef1f07cf73d733bce47c52af
vps07 <vikysingh07860@gmail.com>
2022-10-11 00:29:46 +0530

    Added nav bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Cards from "./components/Cards";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Cards />
     </div>
   );
---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { ImQuotesLeft } from "react-icons/im";
+
+export default function Nav() {
+    return (
+        <div className='nav-bar'>
+            <ImQuotesLeft className='nav-icon' />
+            <p>Quotes App</p>
+        </div>
+    )
+}
