ce7f803bc3e0abe0504b0c00f3d67cfba80e484c
vps07 <vikysingh07860@gmail.com>
2023-01-11 00:08:40 +0530

    added favicon

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -7,23 +7,12 @@ import { CardContext } from "../App";
 
 export default function Cards() {
 
-  const { quotes, getData } = useContext(CardContext);
+  const { quotes } = useContext(CardContext);
 
-  function editHandleClick() {
-    /*  axiosInstance
-       .patch(`/crudapp/${editProp.id}`, {
-         name: inputRef.current.value,
-         quote: textareaRef.current.value,
-       })
-       .then((res) => {
-         // console.log(res?.data);
-       })
-       .catch((err) => console.log(err)); */
-  }
 
   return (
     <>
-      {quotes.length == 0 ? <Loading /> : <div className="container">
+      {quotes.length === 0 ? <Loading /> : <div className="container">
         {quotes.map((data) => (
           <Card
             key={data._id}
